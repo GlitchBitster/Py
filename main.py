40b5959501d2d45415bd6f6d8ff24ccc59a6baa0
Shriram <shriramthangapandian95@gmail.com>
2024-12-25 00:28:17 +0530

    Added supporting files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pathlib as pl
 
 currentPath = pl.Path().resolve()
-print(str(currentPath) + '\\World Bank Indicators.xlsx')
-file = pd.read_excel(str(currentPath) + '\\World Bank Indicators.xlsx')
+print(str(currentPath) + '\\Contents\\World Bank Indicators.xlsx')
+file = pd.read_excel(str(currentPath) + '\\Contents\\World Bank Indicators.xlsx')
 
 print(file.head(1)['Population: Total (count)'])

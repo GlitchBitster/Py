15214e1b596297dcde5da44effda6739b29a285f
Shriram <shriramthangapandian95@gmail.com>
2024-12-29 23:30:48 +0530

    Added logic for creating doc and adding rows to tables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pathlib as pl
+from docx import Document
 from Models.Model import ModelClass
 
 currentPath = pl.Path().resolve()
@@ -8,6 +9,7 @@ file = pd.read_excel(str(currentPath) + '\\Content\\World Bank Indicators.xlsx')
 
 modelInstance = []
 slNo = 0
+
 for _, row in file.tail(5).iterrows():
     model = ModelClass(
         slNo=slNo + 1,
@@ -16,8 +18,25 @@ for _, row in file.tail(5).iterrows():
         mobileSubs = row.get("Business: Mobile phone subscribers"),
         internetSubs = row.get("Business: Internet users (per 100 people)")
     )
-    print(_)
+    slNo = slNo + 1
     modelInstance.append(model)
 
-for row in modelInstance:
-    print(row)
+# for row in modelInstance:
+    # print(row.slNo)
+
+doc = Document(str(currentPath) + "\\Templates\\World Bank Indicators.docx")
+flag = 1
+for modelRow in modelInstance:
+    for table in doc.tables:
+        if flag == 1:
+            table._tbl.remove(table._tbl.tr_lst[1])
+            flag = 0
+        row = table.add_row()
+        row.cells[0].text = str(modelRow.slNo)
+        row.cells[1].text = modelRow.countryName
+        row.cells[2].text = str(modelRow.date)
+        row.cells[3].text = str(modelRow.mobileSubs)
+        row.cells[4].text = str( modelRow.internetSubs)
+        # table.add_row
+
+doc.save(str(currentPath) + "\\Templates\\World Bank Indicators_test.docx")
